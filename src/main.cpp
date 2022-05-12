b26bac8bae8c510b6636f2d1789f4d68918be9a6
李琦（lirich674@gmail.com） <66772096+liuqian62@users.noreply.github.com>
2022-05-12 16:29:29 +0800

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <vector>
+
+
+using namespace std;
+
+
+int main()
+{
+    cout<<"hello world"<<endl;
+    return 0;
+}
